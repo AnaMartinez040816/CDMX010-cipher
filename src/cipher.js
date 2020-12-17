343bd1874c177bb10b574f6e186892a024e3f781
Ana Martinez <ana.martinez.040816@gmail.com>
2020-12-16 23:41:55 -0600

    subiendo original de metodo de cifrado cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,42 @@
 const cipher = {
-  // ...
-};
+  encode: function(offset, string) { 
+           // ...Codificacion
+    let  encodeString = "";
+    let newLetter=" ";
+    let ascciNumber=0
+    let finalNumber=0
+    for (let i = 0;  i < string.length;  i++) {
+      //Capital Letters from 65 to 90
+      ascciNumber=(string.charCodeAt(i))
+      console.log (ascciNumber)
+      finalNumber=(ascciNumber- 65 + offset)%26 + 65;
+     
+     // if (ascciNumber=32) {
+       // finalNumber=32
+      //} 
+
+      //aqui debe cerrse el if
+
+    newLetter=String.fromCharCode(finalNumber);
+
+    encodeString+= newLetter;    
+    }
+    return encodeString;
+  },
+
+  
+  decode: function(offset, string) { 
+          // ...Decodificacion
+    let  decodeString = "";
+    for (let i = 0;  i < string.length;  i++) {  
+      let finalNumber=((string.charCodeAt(i))- 65 - offset)%26 + 65;
+      let newLetter=String.fromCharCode(finalNumber);
+      decodeString+= newLetter;    
+    }
+    return decodeString;
+  }
+
+}
+
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+// Funcion para codificar 
+
+document.getElementById("btnEncode").addEventListener("click", function(){
+    let texto=document.getElementById("string").value;
+    let despl=document.getElementById("offset").value;
+    let offset = parseInt(despl, 10);
+    let codificado=cipher.encode(offset, texto);
+    document.querySelector("#resultado").innerHTML=codificado;
+}); 
+
+document.getElementById("btnDecode").addEventListener("click", function(){
+    let texto=document.getElementById("string").value;
+    let despl=document.getElementById("offset").value;
+    let offset = parseInt(despl, 10);
+    let decodificado=cipher.decode(offset, texto);
+    document.querySelector("#resultado").innerHTML=decodificado;
+}); 
