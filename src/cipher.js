5d5a9d5b1df4cc24810a39de99a4e88e97c8971f
Ana Martinez <ana.martinez.040816@gmail.com>
2020-12-17 14:12:23 -0600

    codifica con espacios

--- src/cipher.js
+++ src/cipher.js
@@ -1,15 +1,18 @@
 const cipher = {
   encode: function (offset, string) {
+    //validando entradas
+    //if (offset === null  &&  string === null){
+     //  throw new TypeError(¨Falta ingresar el mensaje y el desplazamiento)
+    //}
     // ...Codificacion
+      //Capital Letters from 65 to 90  
     let encodeString = "";
     let newLetter = " ";
     let ascciNumber = 0
     let finalNumber = 0
     for (let i = 0; i < string.length; i++) {
-      //Capital Letters from 65 to 90
       ascciNumber = (string.charCodeAt(i))
-      console.log(ascciNumber)
-      if (ascciNumber != 32) {
+      if (ascciNumber != 32) {  // espacio
         finalNumber = (ascciNumber - 65 + offset) % 26 + 65;
       }
       else {
@@ -27,16 +30,32 @@ const cipher = {
 
   decode: function (offset, string) {
     // ...Decodificacion
-    let decodeString = "";
+    let encodeString = "";
+    let newLetter = " ";
+    let ascciNumber = 0
+    let finalNumber = 0
     for (let i = 0; i < string.length; i++) {
-      let finalNumber = ((string.charCodeAt(i)) - 65 - offset) % 26 + 65;
-      let newLetter = String.fromCharCode(finalNumber);
-      decodeString += newLetter;
+
+      ascciNumber = (string.charCodeAt(i))
+      //Capital Letters from 65 to 90      
+      if (ascciNumber != 32) {  // espacio
+        finalNumber = (ascciNumber - 65 - offset) % 26 + 65;
+      }
+      else {
+        finalNumber = 32
+      }
+      //aqui debe cerrse el if
+
+      newLetter = String.fromCharCode(finalNumber);                  
+
+      encodeString += newLetter;
     }
-    return decodeString;
+    return encodeString;
+
+
   }
 
+
+
 }
-
-
 export default cipher;
